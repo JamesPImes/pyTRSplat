0abee17f92fda0c4c2a3e4ce65b335ca0b8240ec
James P. Imes <jamesimes@gmail.com>
2020-10-05 14:20:49 -0700

    added website and shorthand version of the license to _constants

--- pyTRSplat/_constants.py
+++ pyTRSplat/_constants.py
@@ -4,3 +4,22 @@ __version__ = '0.1.1'
 __versionDate__ = '9/23/2020'
 __author__ = 'James P. Imes'
 __email__ = 'jamesimes@gmail.com'
+__website__ = 'https://github.com/JamesPImes/pyTRSplat'
+
+__license__ = (
+    "Copyright (c) 2020, James P. Imes, all rights reserved.\n\n"
+    "pyTRSplat, together with all accompanying programs and modules, is licensed "
+    "under a 'Modified Academic Public License', modified from the OMNeT++ "
+    "license, which was written by Andras Varga (license text is in public "
+    "domain), as obtained at <https://omnetpp.org/intro/license>. This "
+    "modified license generally allows 'non-commercial' use and modification, "
+    "but DISALLOWS ANY COMMERCIAL OR FOR-PROFIT USE, MODIFICATION, AND "
+    "DISTRIBUTION.\n\n"
+    "Read pyTRSplat\\LICENSE.txt for the full terms and conditions, also available "
+    f"in full at <{__website__}>.\n\n"
+    f"Contact <{__email__}> with inquiries regarding commercial or for-profit "
+    "use.\n\n"
+    "Various open-source software and assets are also used by pyTRSplat, and "
+    "their respective copyright and license information is found in the "
+    "'pyTRSplat\\_modulelicenses' directory."
+)
