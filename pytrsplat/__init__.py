21b8443acc544d60a70ed475fc9c6ff982638a21
James P. Imes <jamesimes@gmail.com>
2021-03-11 19:41:50 -0800

    cleaner imports

--- pytrsplat/__init__.py
+++ pytrsplat/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2020, James P. Imes. All rights reserved.
 
 """
-pytrsplat -- A module to generate customizable land plat images from
-PLSS land descriptions ('legal descriptions'), using the pytrs parsing
+pyTRSplat -- A module to generate customizable land plat images from
+PLSS land descriptions ('legal descriptions'), using the pyTRS parsing
 module; and/or from manually-selected lands. Import as a module, or use
 the GUI application ('pytrsplat/pyTRSplat_app_windowed.pyw').
 
@@ -11,25 +11,31 @@ Quick Guide to Module Structure / Packages:
         (or run 'pytrsplat/pyTRSplat_app_windowed.pyw' directly)
 
 These functions and classes from the various packages are all imported
-as main-level objects and functions. But additional documentation can
+as top-level classes and functions. But additional documentation can
 be found in their respective source packages:
-`pytrsplat.plat` -- Generate plat images
+
+``pytrsplat.plat`` -- Generate plat images
     classes:    Plat, MultiPlat
     functions:  text_to_plats()
-`pytrsplat.grid` -- Interpret pytrs.PLSSDesc and pytrs.Tract objects,
+
+``pytrsplat.grid`` -- Interpret pytrs.PLSSDesc and pytrs.Tract objects,
         define how specific lots should be interpreted, manually.
     classes:    SectionGrid, TownshipGrid, LotDefinitions,
         TwpLotDefinitions, LotDefDB
     functions:  tracts_into_twp_grids(), plssdesc_to_twp_grids()
-`pytrsplat.platqueue` -- Streamlined queuing of objects to add to plats
+
+``pytrsplat.platqueue`` -- Streamlined queuing of objects to add to plats
     classes:    PlatQueue, MultiPlatQueue
-`pytrsplat.platsettings` -- Configure plats (size, colors, fonts, etc.)
+
+``pytrsplat.platsettings`` -- Configure plats (size, colors, fonts, etc.)
     classes:    Settings
-`pytrsplat.settingseditor` -- A GUI editor for `platsettings.Settings`
+
+``pytrsplat.settingseditor`` -- A GUI editor for `platsettings.Settings`
     classes:    SettingsEditor (NOTE: use the `launch_settings_editor()`
         function to use this class, unless it is being incorporated into
         an application.)
     functions:  launch_settings_editor()
+
 `pytrsplat.utils` -- Misc. utils
     functions:  filter_tracts_by_twprge()
 """
@@ -48,14 +54,25 @@ def version():
     """Return the current version and version date as a string."""
     return f'v{__version__} - {__versionDate__}'
 
-from pytrsplat.plat import Plat, MultiPlat
-from pytrsplat.plat import text_to_plats
-from pytrsplat.grid import TownshipGrid, SectionGrid
-from pytrsplat.grid import LotDefinitions, TwpLotDefinitions, LotDefDB
-from pytrsplat.grid import tracts_into_twp_grids, plssdesc_to_twp_grids
+
+from pytrsplat.plat import (
+    Plat,
+    MultiPlat,
+    text_to_plats,
+)
+
+from pytrsplat.grid import (
+    TownshipGrid,
+    SectionGrid,
+    LotDefinitions,
+    TwpLotDefinitions,
+    LotDefDB,
+    tracts_into_twp_grids,
+    plssdesc_to_twp_grids,
+)
+
 from pytrsplat.platsettings import Settings
-from pytrsplat.settingseditor import SettingsEditor
-from pytrsplat.settingseditor import launch_settings_editor
+from pytrsplat.settingseditor import SettingsEditor, launch_settings_editor
 from pytrsplat.platqueue import PlatQueue, MultiPlatQueue
 from pytrsplat.utils import filter_tracts_by_twprge
 
