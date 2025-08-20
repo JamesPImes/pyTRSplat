b4a83f3c7ca225e1346468a508a140b28314f293
James Imes <jamesimes@gmail.com>
2025-08-19 20:14:04 -0600

    get rid of deprecated modules, etc.

--- pytrsplat/gui/imgdisplay/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2020, James P. Imes, all rights reserved.
-
-"""
-A tkinter Frame for displaying an PIL.Image object.
-"""
-
-from .imgdisplay import ScrollResizeDisplay
--- pytrsplat/plat_gen/_plat/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2020, James P. Imes. All rights reserved.
-
-"""
-Generate the _plat images.
-"""
-
-from .plat import Plat, MultiPlat
-from .plat import text_to_plats
