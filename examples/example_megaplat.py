e952891642e85dfad004eba62e1cf0399d8eb330
James Imes <jamesimes@gmail.com>
2025-08-20 00:44:10 -0600

    sample scripts and results

---
+++ examples/example_megaplat.py
@@ -0,0 +1,39 @@
+import pytrsplat
+
+land_description = '''Township 154 North, Range 97 West
+Section 1: Lots 1 - 3, S/2N/2
+Section 5: Lot 4, The South Half of the Northwest Quarter, and The Southwest Quarter
+Section 6: Lots 1 - 5, E/2 of Lots 6 and 7, S/2NE/4, SE/4NW/4, E/2SW/4, SE/4
+Section 13: That portion of the E/2 lying north of the river and west of the private road right-of-way as more particularly described in Book 1234 / Page 567, recorded on January 1, 1964 in the records of Example County, as amended in that Right-of-Way Amendment Agreement dated December 10, 1987, recorded on December 11, 1987 as Document No. 1987-1234567 of the records of Example County.
+Section 14: NE/4, NE/4NW/4, S/2NW/4NW/4
+Section 29: Lots 1, 2, 3
+Section 31: Lot 1, N/2NE/4, SE/4NE/4
+
+Township 155 North, Range 97 West
+Section 3: Lots 3, 4, S/2NW/4
+Section 27: East 30 ft of the W/2
+
+Township 156 North, Range 94 West
+Section 4: ALL
+'''
+
+mega_plat = pytrsplat.MegaPlat()
+# Use preset settings.
+# Note: HIGHLY recommend using a 'megaplat_default' or 'megaplat_s'.
+mega_plat.settings = pytrsplat.Settings.preset('megaplat_default')
+# Load a lot definer from .csv f ile.
+mega_plat.lot_definer.load_from_csv(r"sample_lot_definitions_154n97w.csv")
+# Assume 'standard' 40-acre lots (in sections along north and west of township)
+mega_plat.lot_definer.allow_defaults = True
+mega_plat.lot_definer.standard_lot_size = 40
+
+# Add the above land description, using the specified pytrs config string.
+mega_plat.add_description(land_description, pytrs_config='default_ns.n,default_ew.w')
+mega_plat.execute_queue()
+# Output the results, and save them to a PNG.
+only_image = mega_plat.output(
+    fp=r'./results/example_megaplat_results.png',
+)
+
+# View the result in whichever app is your default image viewer.
+only_image.show()
---
+++ examples/example_plat.py
@@ -0,0 +1,27 @@
+import pytrsplat
+
+land_description = "T8S-R58W, Sec 1: Lot 1, E/2 of Lot 2, S/2N/2, Sec 2: E/2, Sec 11: ALL"
+
+# If we didn't specify Twp/Rge here, it would be added when we add the first tract.
+plat = pytrsplat.Plat(twp='8s', rge='58w')
+# Use preset settings, but modify it to write lot numbers.
+plat.settings = pytrsplat.Settings.preset('default')
+plat.settings.write_lot_numbers = True
+# Assume 'standard' 80-acre lots (in sections along north and west of township)
+plat.lot_definer.allow_defaults = True
+# For this Twp/Rge, we assume lots are standardized to 80 acres, common in parts of Colorado.
+plat.lot_definer.standard_lot_size = 80
+
+# Add the above land description, using the specified pytrs config string.
+# NOTE: If we tried to add lands outside of T8S-R58W, this would raise a ValueError.
+# If we need to plat lands across township boundaries, use a PlatGroup or MegaPlat instead.
+plat.add_description(land_description, pytrs_config='default_ns.s,default_ew.w')
+
+plat.execute_queue()
+# Output the results, and save them to a PNG. (Could also create .PDF, .TIFF, etc.).
+only_image = plat.output(
+    fp=r'./results/example_plat_results.png',
+)
+
+# View the result in whichever app is your default image viewer.
+only_image.show()
