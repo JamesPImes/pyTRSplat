d104ccfdb1909e1349feca35908575e9da2bd7c8
James Imes <jamesimes@gmail.com>
2025-08-20 21:41:58 -0600

    test Settings init

---
+++ tests/test_settings.py
@@ -0,0 +1,19 @@
+import unittest
+
+try:
+    from pytrsplat import Settings
+except ImportError:
+    import sys
+
+    sys.path.append('../')
+    from pytrsplat import Settings
+
+class PlatTests(unittest.TestCase):
+    def test_init(self):
+        settings = Settings()
+        # Default fill, transparent blue.
+        self.assertEqual(settings.qq_fill_rgba, (0, 0, 255, 100))
+
+
+if __name__ == '__main__':
+    unittest.main()
