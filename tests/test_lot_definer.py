1eab4dfa9b1458e4503f39c015f31f25997a2903
James Imes <jamesimes@gmail.com>
2025-08-24 00:18:56 -0600

    test: LotDefiner.read_csv()

--- tests/test_lot_definer.py
+++ tests/test_lot_definer.py
@@ -14,16 +14,28 @@ RESOURCE_DIR = Path(r"_resources")
 
 class LotDefinerTests(unittest.TestCase):
 
+    csv_fp: Path = RESOURCE_DIR / 'test_lot_definitions.csv'
+
     def test_init(self):
         ld = LotDefiner(allow_defaults=True)
         self.assertEqual(ld.allow_defaults, True)
 
     def test_from_csv(self):
-        fp = RESOURCE_DIR / 'test_lot_definitions.csv'
-        ld = LotDefiner.from_csv(fp)
+        ld = LotDefiner.from_csv(self.csv_fp)
         self.assertEqual(len(ld.definitions.keys()), 2)
         self.assertTrue(ld.definitions.get('154n97w08', None) is not None)
         self.assertTrue(ld.definitions.get('12s58e14', None) is not None)
+
+    def test_read_csv(self):
+        ld = LotDefiner()
+        ld.define_lot('154n97w07', 1, 'NENE')
+        self.assertEqual(len(ld.definitions.keys()), 1)
+        ld.read_csv(self.csv_fp)
+        self.assertTrue(ld.definitions.get('154n97w07', None) is not None)
+        self.assertTrue(ld.definitions.get('154n97w08', None) is not None)
+        self.assertTrue(ld.definitions.get('12s58e14', None) is not None)
+        self.assertEqual(len(ld.definitions.keys()), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
