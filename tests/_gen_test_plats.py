e7d0e90434a18967346d77a1c46fba66adc768e3
James Imes <jamesimes@gmail.com>
2025-08-25 01:45:57 -0600

    tests: Plat outputs

---
+++ tests/_gen_test_plats.py
@@ -0,0 +1,199 @@
+"""
+Generate expected images for ``Plat``.
+
+Must then visually verify they are all correct before running the unit
+tests.
+"""
+
+from pathlib import Path
+
+try:
+    from pytrsplat import Plat, Settings
+    from ._utils import (
+        prepare_settings,
+        get_test_settings_for_plat,
+        images_match,
+        image_matches_existing,
+        add_docstring,
+        write_if_new_single,
+        gen_all_test_plats,
+        RESOURCES_DIR,
+        TEST_RESULTS_DIR,
+    )
+
+except ImportError:
+    import sys
+
+    sys.path.append('../')
+    from pytrsplat import Plat, Settings
+    from _utils import (
+        prepare_settings,
+        get_test_settings_for_plat,
+        images_match,
+        image_matches_existing,
+        add_docstring,
+        write_if_new_single,
+        gen_all_test_plats,
+        RESOURCES_DIR,
+        TEST_RESULTS_DIR,
+    )
+
+__all__ = [
+    'FILENAME_TO_GENFUNC',
+    'plat2tracts_nodefaults',
+    'plat2tracts_defaults40ac',
+    'plat2tracts_defaults80ac',
+    'plat2tracts_lotnums_defaults40ac',
+    'plat2tracts_lotnums_defaults80ac',
+    'plat2tracts_writetracts',
+    'plat2tracts_writetracts_lotnums',
+    'plat3tracts_writetracts_2written',
+    'plat1tract_noheader',
+]
+
+DEFAULT_OUT_DIR = RESOURCES_DIR / 'expected_images' / 'plat'
+DESC_1 = 'T154N-R97W Sec 1: Lots 1, 2, S/2N/2'
+DESC_2 = 'T154N-R97W Sec 6: Lots 2 - 4'
+DESC_3 = 'T154N-R97W Sec 8: N/2S/2NW/4'
+
+
+@add_docstring(
+    'Plat - Two tracts. No default lots.',
+    DESC_1, DESC_2)
+def plat2tracts_nodefaults(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    plat = Plat(settings=settings)
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Two tracts. 40-acre default lots (not written).',
+    DESC_1, DESC_2)
+def plat2tracts_defaults40ac(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 40
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Two tracts. 80-acre default lots (not written).',
+    DESC_1, DESC_2)
+def plat2tracts_defaults80ac(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 80
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Two tracts. 40-acre default lots (written).',
+    DESC_1, DESC_2)
+def plat2tracts_lotnums_defaults40ac(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_lot_numbers = True
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 40
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Two tracts. 80-acre default lots (written).',
+    DESC_1, DESC_2)
+def plat2tracts_lotnums_defaults80ac(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_lot_numbers = True
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 80
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring('Plat - Two tracts. Tracts written in footer. No default lots.', DESC_1, DESC_2)
+def plat2tracts_writetracts(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_tracts = True
+    plat = Plat(settings=settings)
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Two tracts. Tracts written to footer. 40-acre default lots (written).',
+    DESC_1, DESC_2)
+def plat2tracts_writetracts_lotnums(
+        fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_tracts = True
+    settings.write_lot_numbers = True
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 40
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring(
+    'Plat - Three tracts. Tracts written to footer (except 3rd due to margin).',
+    DESC_1, DESC_2, DESC_3)
+def plat3tracts_writetracts_2written(
+        fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_tracts = True
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 40
+    plat.add_description(DESC_1)
+    plat.add_description(DESC_2)
+    plat.add_description(DESC_3)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+@add_docstring('Plat - One tract. No header.', DESC_1)
+def plat1tract_noheader(fn: str, out_dir: Path = DEFAULT_OUT_DIR, override=False):
+    settings = get_test_settings_for_plat()
+    settings.write_header = False
+    plat = Plat(settings=settings)
+    plat.lot_definer.allow_defaults = True
+    plat.lot_definer.standard_lot_size = 40
+    plat.add_description(DESC_1)
+    plat.execute_queue()
+    return write_if_new_single(out_dir / fn, plat, override)
+
+
+# Create a dict of .png filenames and their corresponding func, for all funcs.
+exclude = (
+    'FILENAME_TO_GENFUNC',
+)
+loc = locals()
+FILENAME_TO_GENFUNC = {
+    f"{func_name}.png": loc[func_name]
+    for func_name in __all__
+    if func_name not in exclude
+}
+
+if __name__ == '__main__':
+    gen_all_test_plats(FILENAME_TO_GENFUNC, check_new=True, override=False)
--- tests/test_plat.py
+++ tests/test_plat.py
@@ -1,18 +1,71 @@
 import unittest
+from pathlib import Path
 
 try:
-    from pytrsplat import Plat
+    from pytrsplat import Plat, Settings
+    from ._utils import (
+        prepare_settings,
+        get_test_settings_for_plat,
+        compare_tests_with_expected,
+        RESOURCES_DIR,
+        TEST_RESULTS_DIR,
+    )
+    from ._gen_test_plats import (
+        FILENAME_TO_GENFUNC,
+    )
 except ImportError:
     import sys
 
     sys.path.append('../')
-    from pytrsplat import Plat
+    from pytrsplat import Plat, Settings
+    from ._utils import (
+        prepare_settings,
+        get_test_settings_for_plat,
+        compare_tests_with_expected,
+        RESOURCES_DIR,
+        TEST_RESULTS_DIR,
+    )
+    from ._gen_test_plats import (
+        FILENAME_TO_GENFUNC,
+    )
 
-class PlatTests(unittest.TestCase):
+
+class TestPlatBehavior(unittest.TestCase):
+    expected_dir: Path = RESOURCES_DIR / 'expected_images' / 'plat'
+    out_dir: Path = TEST_RESULTS_DIR / 'plat'
+
     def test_init(self):
         plat = Plat('154n', '97w')
         self.assertEqual(plat.twp, '154n')
         self.assertEqual(plat.rge, '97w')
+
+
+class TestPlatOutput(unittest.TestCase):
+    """Test the output results for Plat."""
+
+    expected_dir: Path = RESOURCES_DIR / 'expected_images' / 'plat'
+    out_dir: Path = TEST_RESULTS_DIR / 'plat'
+
+    @classmethod
+    def setUpClass(cls):
+        prepare_settings()
+
+    def test_matching_outputs(self):
+        """
+        Generate test plats and compare their output to expected results.
+
+        (See ``_gen_test_plats.py`` for the inputs/settings for the
+        various plats.)
+        """
+        mismatched = compare_tests_with_expected(
+            FILENAME_TO_GENFUNC,
+            expected_dir=self.expected_dir,
+            out_dir=self.out_dir,
+        )
+        msg = {f"Mismatched output(s):\n{'\n\n'.join(mismatched)}"}
+        self.assertTrue(len(mismatched) == 0, msg)
+        return None
+
 
 if __name__ == '__main__':
     unittest.main()
