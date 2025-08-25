f4c9291a2467617c93a62266b674631aabe94db4
James Imes <jamesimes@gmail.com>
2025-08-25 02:36:48 -0600

    generate test outputs for all plat classes

---
+++ tests/gen_test_all_classes.py
@@ -0,0 +1,27 @@
+try:
+    import _gen_test_plats
+    import _gen_test_megaplats
+    import _gen_test_platgroups
+    from _utils import gen_all_test_plats
+
+except ImportError:
+    import sys
+
+    sys.path.append('../')
+    import _gen_test_plats
+    import _gen_test_megaplats
+    import _gen_test_platgroups
+    from _utils import gen_all_test_plats
+
+
+def gen_test_outputs(check_new=False, override=False):
+    for filename_to_genfunc in (
+            _gen_test_plats.FILENAME_TO_GENFUNC,
+            _gen_test_megaplats.FILENAME_TO_GENFUNC,
+            _gen_test_platgroups.FILENAME_TO_GENFUNC
+    ):
+        gen_all_test_plats(filename_to_genfunc, check_new, override)
+
+
+if __name__ == '__main__':
+    gen_test_outputs(check_new=True)
