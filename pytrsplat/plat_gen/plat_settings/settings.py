e008459e966d9aee897b88f7ce19531522ea6ded
James Imes <jamesimes@gmail.com>
2025-08-26 00:29:54 -0600

    fix: put initial slash back in macOS abs paths

--- pytrsplat/plat_gen/plat_settings/settings.py
+++ pytrsplat/plat_gen/plat_settings/settings.py
@@ -320,6 +320,9 @@ class Settings:
             path_components = [v]
             if '/' in v:
                 path_components = v.split('/')
+                if v[0] == '/':
+                    # Put macOS's initial slash back in.
+                    v.insert(0, '/')
             v = os.path.join(*path_components)
             d[att] = v
         return cls.from_dict(d)
